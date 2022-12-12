c61a52a97f6cf53a234c5fccaa3a3eb7d32099f3
A.S.Rawat <adityasrwt5@gmail.com>
2022-12-12 18:25:42 +0530

    Line Rider

--- Line Rider/Assets/Scripts/Line.cs
+++ Line Rider/Assets/Scripts/Line.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
 
 public class Line : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D eCol;
+    List<Vector2> points;
+
+    public void updateLine(Vector2 mousePos)
     {
-        
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            setPoint(mousePos);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), mousePos) > .1f)
+        {
+            setPoint(mousePos);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void setPoint(Vector2 point)
     {
-        
+        points.Add(point);
+
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPosition(points.Count - 1, point);
+        if (points.Count > 1)
+        {
+            eCol.points = points.ToArray();
+        }
     }
+
+
 }
